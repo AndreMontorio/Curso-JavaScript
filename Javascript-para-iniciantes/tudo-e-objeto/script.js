525f11ee9b12472fae06d795e43fc368e9188502
André Montório Souto <andre.montorio@consinco.com.br>
2019-04-23 08:36:30 -0300

    Tudo é objeto incompleto

---
+++ Javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,10 @@
+// TUDO É OBJETO
+
+// Strings, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
+var nome = 'André';
+
+nome.length; // 5
+nome.charAt(1); // 'n'
+nome.replace('ré', 'rei'); // 'Andrei'
+nome; // 'André'
+// Uma string herda propriedades e métodos do construtor String()
